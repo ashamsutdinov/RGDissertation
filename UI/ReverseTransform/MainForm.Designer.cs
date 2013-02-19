51155ce54c8fba9b1f1c221277cab42f353a15ec
Aydario Shamsutdinoff <ashamsutdinov@igsystems.ru>
2013-02-20 01:23:27 +0400

    new proj

---
+++ UI/ReverseTransform/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ReverseTransform
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+      this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+      this.pictureBox = new System.Windows.Forms.PictureBox();
+      this.menuStrip1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "mainMenu";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+      this.fileToolStripMenuItem.Text = "File";
+      // 
+      // toolStripSeparator1
+      // 
+      this.toolStripSeparator1.Name = "toolStripSeparator1";
+      this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+      // 
+      // exitToolStripMenuItem
+      // 
+      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+      this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.exitToolStripMenuItem.Text = "Exit";
+      this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+      // 
+      // helpToolStripMenuItem
+      // 
+      this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+      this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+      this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+      this.helpToolStripMenuItem.Text = "Help";
+      // 
+      // aboutToolStripMenuItem
+      // 
+      this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+      this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.aboutToolStripMenuItem.Text = "About";
+      // 
+      // statusStrip1
+      // 
+      this.statusStrip1.Location = new System.Drawing.Point(0, 740);
+      this.statusStrip1.Name = "statusStrip1";
+      this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+      this.statusStrip1.TabIndex = 1;
+      this.statusStrip1.Text = "statusBar";
+      // 
+      // pictureBox
+      // 
+      this.pictureBox.Location = new System.Drawing.Point(12, 27);
+      this.pictureBox.Name = "pictureBox";
+      this.pictureBox.Size = new System.Drawing.Size(760, 710);
+      this.pictureBox.TabIndex = 2;
+      this.pictureBox.TabStop = false;
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(784, 762);
+      this.Controls.Add(this.pictureBox);
+      this.Controls.Add(this.statusStrip1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "MainForm";
+      this.Text = "Revers RG-Transformation";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    private System.Windows.Forms.StatusStrip statusStrip1;
+    private System.Windows.Forms.PictureBox pictureBox;
+  }
+}
+
---
+++ UI/ReverseTransform/MainForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReverseTransform
+{
+  public partial class MainForm : Form
+  {
+    internal class Pixel
+    {
+      public int X { get; set; }
+
+      public int Y { get; set; }
+
+      public Color Color { get; set; }
+    }
+
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    private Queue<Pixel> Pixels { get; set; }
+
+    private Task GrTask { get; set; }
+
+    protected override void OnShown(EventArgs e)
+    {
+      base.OnShown(e);
+      Pixels = new Queue<Pixel>();
+      InitializeArea();
+      InitializePainter();
+    }
+
+    private void InitializePainter()
+    {
+      GrTask = new Task(() =>
+      {
+        while (Visible)
+        {
+          var bmp = pictureBox.Image as Bitmap;
+          if (bmp == null || !Pixels.Any())
+            continue;
+          lock (bmp)
+          {
+            var p = Pixels.Dequeue();
+            bmp.SetPixel(p.X, p.Y, p.Color);
+          }
+        }
+      });
+      GrTask.Start();
+    }
+
+    private void InitializeArea()
+    {
+      var r = new Random();
+      pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+      for (var i = 0; i < pictureBox.Width; i++)
+      {
+        for (var j = 0; j < pictureBox.Height; j++)
+        {
+          SetPixel(i, j, Color.FromArgb(r.Next()));
+        }
+      }
+    }
+
+    private void SetPixel(int x, int y, Color color)
+    {
+      Pixels.Enqueue(new Pixel { X = x, Y = y, Color = color });
+    }
+
+    private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+  }
+}
