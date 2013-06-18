a5803ebc78d4a6d72b90fa55324da4f414351def
Aydario Shamsutdinoff <ashamsutdinov@igsystems.ru>
2013-06-19 00:28:00 +0400

    ReverseTransformRG

---
+++ UI/ReverseTransformRG/TrackPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ReverseTransform
+{
+  public partial class TrackPoint : Form
+  {
+    private readonly MainForm _form;
+
+    public TrackPoint(MainForm form, int max)
+    {
+      InitializeComponent();
+      trackBar.Minimum = 0;
+      trackBar.Maximum = max - 1;
+      trackBar.Value = 0;
+      _form = form;
+    }
+
+    private void TrackBarValueChanged(object sender, EventArgs e)
+    {
+      _form.PointNumber = trackBar.Value;
+      _form.RedrawScene();
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+      base.OnClosed(e);
+      _form.CleanPoint(this, e);
+    }
+  }
+}
